8d7d6072e800a7a7cb58230b88df5c3fc7446626
Tiago D. Teixeira <tiagoadmstz@yahoo.com.br>
2020-05-18 10:16:45 -0300

    18/05/2020

--- src/main/java/com/wallet/WalletApplication.java
+++ src/main/java/com/wallet/WalletApplication.java
@@ -2,12 +2,16 @@ package com.wallet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@ComponentScan
+@EnableJpaRepositories
 @SpringBootApplication
 public class WalletApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WalletApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WalletApplication.class, args);
+    }
 
 }
--- src/test/java/com/wallet/HelloWorld.java
+++ src/test/java/com/wallet/HelloWorld.java
@@ -1,18 +1,16 @@
 package com.wallet;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 public class HelloWorld {
-	
-	@Test
-	 public void testHelloWorld() {
-		 assertEquals(1,1);
-	 }
+
+    @Test
+    public void testHelloWorld() {
+        assertEquals(1, 1);
+    }
+
 }
