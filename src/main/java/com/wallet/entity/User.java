eed60906d938d8775692463d484de7d96f6c0e44
Caio Jano <caiojano@hotmail.com>
2020-05-21 14:47:53 -0300

    Finalizado repositório do usuário

--- src/main/java/com/wallet/entity/User.java
+++ src/main/java/com/wallet/entity/User.java
@@ -9,9 +9,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Data
+@Getter
+@Setter
 public class User implements Serializable{
 	
 	/**
@@ -23,8 +27,35 @@ public class User implements Serializable{
 	 private Long id;
 	@Column(nullable = false)
 	private String password;
-	@Column(nullable = false)
-	private String name;
 	@Column(nullable = false) 
-	private String email; 
+	private String name;
+	@Column(nullable = false)  
+	private String email;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	} 
 }
--- src/test/java/com/wallet/repository/UserRepositoryTest.java
+++ src/test/java/com/wallet/repository/UserRepositoryTest.java
@@ -1,7 +1,13 @@
 package com.wallet.repository;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import java.util.Optional;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,18 +20,43 @@ import com.wallet.entity.User;
 @SpringBootTest
 public class UserRepositoryTest {
 
+	private static final String EMAIL = "email@teste.com";
+	
 	@Autowired
 	UserRepository repository;
 	
+	@Before
+	public void setUp() {
+		User u = new User();
+		u.setName("Set up User");
+		u.setPassword("Senha123");
+		u.setEmail(EMAIL);
+		
+		repository.save(u);
+	}
+	
+	@After
+	public void tearDown() {
+		
+		repository.deleteAll();
+	}
+	
 	@Test
 	public void testSave() {
-		User u = new User();
-		u.setName("Teste");
-		u.setPassword("123456");
-		u.setEmail("teste@teste.com");
+		User x = new User();
+		x.setName("Teste");
+		x.setPassword("123456");
+		x.setEmail("teste@teste.com");
 		
-		User response = repository.save(u);
+		User response = repository.save(x);
 		
 		assertNotNull(response);
+	}
+	
+	public void testFindByEmail() {
+		Optional<User> response = repository.findByEmailEquals(EMAIL);
+		
+		assertTrue(response.isPresent());
+		assertEquals(response.get().getEmail(), EMAIL);
 	}
 }
